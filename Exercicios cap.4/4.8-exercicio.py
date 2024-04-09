f41d6324cca1a97963bd4e5ac20b5b58d4044622
Lucas Matheus <ffmatheus6@GMAIL.COM>
2024-04-09 17:35:34 -0300

    Exercicios cap.4

--- Exercicios cap.4/4.8-exercicio.py
+++ Exercicios cap.4/4.8-exercicio.py
@@ -1,4 +1,4 @@
-#criar uma tabela de multiplicação com todos os elementos do array.
+#  criar uma tabela de multiplicação com todos os elementos do array.
 #  Assim, caso o seu array seja [2, 3, 7, 8, 10], você primeiro multiplicará cada elemento por 2.
 #  Depois, multiplicará cada elemento por 3 e então por 7, e assim por diante.
 
